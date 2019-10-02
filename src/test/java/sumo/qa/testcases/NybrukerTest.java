2c930f62e9ef7224c913f67e9369032af9a599a4
havsaran <havsaran@gmail.com>
2019-10-02 10:21:22 +0200

    odl file-new repo

--- src/test/java/sumo/qa/testcases/NybrukerTest.java
+++ src/test/java/sumo/qa/testcases/NybrukerTest.java
@@ -35,8 +35,7 @@ public class NybrukerTest extends TestBase {
 	@BeforeMethod
 	public void setUp() throws InterruptedException {
 		initialization();
-		
-		nybruker = new Nybruker();
+				nybruker = new Nybruker();
 		}
 
 	@Test
